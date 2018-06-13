592a9ca163cb0625224539858875d3b9761cb194
mattt <mace.matt@umail.iu.edu>
2018-06-13 14:38:33 -0500

    Added Marquee, styles

--- src/index.js
+++ src/index.js
@@ -3,10 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
+class SlideMarquee extends Component {
+  render(){
+    return(
+      <section className="music-marquee">
+      <div className="headings">
+      {this.props.mp3Artist} - {this.props.children} ({this.props.mp3Genre})
+      </div>
+      </section>
+    );
+  }
+}
+
 class MP3Player extends Component {
   render(){
     return(
+      <div className="audio-contain">
       <h1>{this.props.title}</h1>
+      <SlideMarquee mp3Artist="Artsist Name" mp3Genre="Hip Hop">What What</SlideMarquee>
+      </div>
     );
   }
 }
